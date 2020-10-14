3634d602044a85a71ec633b8d014f27991bb9029
SunainaSP <sunainanandpal@gmail.com>
2020-10-14 22:56:29 +0530

    First grayscale Code

---
+++ GrayScale.py
@@ -0,0 +1,10 @@
+import cv2
+img = cv2.imread('Sample2.jpg')
+cv2.imshow('Original', img)
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("New Image Grayscale", gray_img)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
