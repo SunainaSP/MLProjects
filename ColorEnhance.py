41d725e62e752b6f6797f7fa01ebc3b64bbc69e1
SunainaSP <sunainanandpal@gmail.com>
2020-10-15 19:32:02 +0530

    Replaced single to double quotes

--- ColorEnhance.py
+++ ColorEnhance.py
@@ -1,4 +1,4 @@
 from PIL import Image, ImageEnhance
-img = Image.open('Sample3.jpeg')
+img = Image.open("Sample3.jpeg")
 img_con = ImageEnhance.Bri(img)
 img_con.enhance(1.0).show("100% more contrast")
